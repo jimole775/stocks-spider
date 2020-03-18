ab9750f75fe0b48bbe743dcc862c47cf0173fde7
Rongxis <727346595@qq.com>
2020-03-18 14:54:06 +0800

    Update index.js

--- src/app/base/sniff-daily-deals/index.js
+++ src/app/base/sniff-daily-deals/index.js
@@ -6,13 +6,14 @@
  */
 import fs from 'fs'
 const { recordPeerDeal } = require('./record-peer-deal')
-const { readFile, batchLink, hasUnlinkItems } = require(`${global.srcRoot}/utils`)
-const baseData = readFile(`${global.srcRoot}/db/warehouse/base.json`)
-const urlModel = readFile(`${global.srcRoot}/url-model.yml`)
-const peerDealReg = new RegExp(urlModel.api.peerDealReg, 'g')
-const allStocks = JSON.parse(baseData ? baseData.data : {})
+const { readFile, batchLink, hasUnlinkItems, dateFormat } = require(`${global.srcRoot}/utils`)
 export async function sniffDailyDeals() {
+  const baseData = readFile(`${global.srcRoot}/db/warehouse/base.json`)
+  const urlModel = readFile(`${global.srcRoot}/url-model.yml`)
+  const peerDealReg = new RegExp(urlModel.api.peerDealReg, 'g')
+  const allStocks = JSON.parse(baseData ? baseData.data : {})
   const recordDir = `${global.srcRoot}/db/warehouse/peer-deals/${global.finalDate}`
+  if (!canContinue()) return false
   const urls = allStocks.map(item => {
     return urlModel.model.PeerDeal
       .replace('[stockCode]', item.code)
@@ -21,6 +22,7 @@ export async function sniffDailyDeals() {
 
   const unlinkItems = hasUnlinkItems(urls, recordDir)
   console.log('peer-deals', unlinkItems.length)
+  
   // 每日交易详情会以日期为目录区分，
   // 所以，如果当前目录的文件数如果饱和，没必要再进行抓取
   unlinkItems.length && batchLink(unlinkItems, {
@@ -35,4 +37,15 @@ export async function sniffDailyDeals() {
       if (unlinkItems.length) batchLink(unlinkItems, this)
     }
   })
+}
+
+function canContinue() {
+  const pass = true
+  const curDate = dateFormat('YYYY-mm-dd', new Date())
+  // 如果最后的交易日是APP运行的当天，那么，15点钟收盘之前，recordPeerDeal都不要运行
+  // 这样做可以让 星期六 的任何时间都可以正常运行 recordPeerDeal
+  if (global.finalDate === curDate && new Date().getHours() < 15) {
+    pass = false
+  }
+  return pass
 }
