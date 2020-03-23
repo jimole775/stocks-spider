f3d71cfcbdc0fb206905fde44d84a4c84c8bcdb8
Rongxis <727346595@qq.com>
2020-03-23 18:42:54 +0800

    Update run.js

--- test/run.js
+++ test/run.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 function run() {
-  const dir = './db'
+  const dir = './test/db'
   const files = fs.readdirSync(dir)
   // p: 价格
   // v: 成交数
@@ -17,46 +17,69 @@ function run() {
 function cacal(store, fileData) {
   const dayItem = {}
   const simpItem = {}
-  let min = 0
-  let max = 1
-  let sum = 0
+  let analyze = {}
+  let min_p = 99999
+  let max_p = 0
+  let sum_v = 0
   let start_p = 0 // 开盘价
   let end_p = 0 // 收盘价
   let diff_p = 0 // 振幅差价
-  let isDownShadow = false // 下影线，开收盘价格相差在1%以内，并且当日最【低】价超过开盘价3-5%
-  let isUpShadow = false // 上影线，开收盘价格相差在1%以内，并且当日最【高】价超过开盘价3-5%
-  let isCrossShadow = false // 十字星，开收盘价格相差在1%以内，并且当日最【高】，最【低】价超过开盘价3-5%
   for (const { p, t, v } of fileData.data) {
     // 9:25分是开盘价
     if(/^9250\d$/.test(t)) {
       start_p = p
     }
-    min = min > p ? p : min
-    max = max < p ? p : max
-    sum += v
+    min_p = min_p > p ? p : min_p
+    max_p = max_p < p ? p : max_p
+    sum_v += v
   }
 
   // 直接获取最后一个价位
   end_p = fileData.data[fileData.data.length - 1].p
 
   // 振幅差价
-  diff_p = max - min
+  diff_p = max_p - min_p
+
+  analyze = analyzing(min_p, max_p, start_p, end_p, sum_v)
 
   // 以百分比计算
   for (const { p, v } of fileData.data) {
     if (!dayItem[p]) {
-      dayItem[p] = (v / sum) * 100
+      dayItem[p] = (v / sum_v) * 100
     } else {
-      dayItem[p] += (v / sum) * 100
+      dayItem[p] += (v / sum_v) * 100
     }
   }
   
   for (const [key, val] of Object.entries(dayItem)) {
-
     // if (val > 1) simpItem[key] = val.toFixed(2) + '%'
     // console.log(aaa)
   }
-  store[fileData.date] = simpItem
+
+  store[fileData.date] = { simpItem, analyze }
+}
+
+function analyzing(min_p, max_p, start_p, end_p, sum_v) {
+  let isDownShadow = false // 下影线，开收盘价格相差在1%以内，并且当日最【低】价超过开盘价3-5%
+  let isUpShadow = false // 上影线，开收盘价格相差在1%以内，并且当日最【高】价超过开盘价3-5%
+  let isCrossShadow = false // 十字星，开收盘价格相差在1%以内，并且当日最【高】，最【低】价超过开盘价3-5%
+  const res = { min_p, max_p, start_p, end_p, sum_v, isDownShadow, isUpShadow, isCrossShadow }
+  if (mostEqual(end_p, start_p, 0.01)) {
+    if (max_p - start_p >= start_p * 0.03) {
+      res['isUpShadow'] = true
+    }
+    if (start_p - min_p >= start_p * 0.03) {
+      res['isDownShadow'] = true
+    }
+    if (mostEqual(start_p - min_p, max_p - start_p, 0.01)) {
+      res['isCrossShadow'] = true
+    }
+  }
+  return res
+}
+
+function mostEqual(a = 0, b = 0, range = 0) {
+  return a >= b && a <= b * (1 + range) || a <= b && a * (1 + range) >= b
 }
 
 run()
