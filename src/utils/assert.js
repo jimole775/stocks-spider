bf0df0985ac722bbdefbe67b342cb9ae1c4d4001
Rongxis <727346595@qq.com>
2020-04-02 16:02:19 +0800

    rangeEqual定性为断言方法

--- src/utils/assert.js
+++ src/utils/assert.js
@@ -37,3 +37,7 @@ export function isCSSUrl(src) {
 export function isJSUrl(src) {
   return /\.(js)(\?.+)?$/.test(src)
 }
+
+export function rangeEqual(a = 0, b = 0, range = 0) {
+  return a >= b && a <= b * (1 + range) || a <= b && a * (1 + range) >= b
+}
