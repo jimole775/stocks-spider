4f4c7a98e0983d3312b579b48e6cae12e7a93ce9
Andy-Super <727346595@163.com>
2020-04-05 18:42:51 +0800

    merge --has-refresh-links

--- src/utils/has-refresh-links.js
+++ src/utils/has-refresh-links.js
@@ -15,13 +15,10 @@ export function hasRefreshLinks(links, recordDir) {
     if (!fs.existsSync(recordDir)) return s([])
 
     const expireFiles = []
-    // 如果被访问的第一个文件有时间戳，
-    // 那么，默认所有文件都有时间戳，
-    // 并对全部文件都进行一次时间戳的验证
     for (const file of files) {
       const data = await readFile(path.join(recordDir, file))
       // 当前时间对比文件的时间戳，不超过一天，就不需要进行重新采集
-      if (data && new Date().getTime() - data.date < 24 * 60 * 60 * 1000) {
+      if (data && new Date(global.finalDate).getTime() - data.date < 24 * 60 * 60 * 1000) {
         continue
       }
       expireFiles.push(file)
