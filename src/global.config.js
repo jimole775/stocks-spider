a19810478b7ce06a56732513f7fbb9e0a204f4c2
Andy-Super <727346595@163.com>
2020-06-03 00:56:31 +0800

    update

--- src/global.config.js
+++ src/global.config.js
@@ -7,7 +7,7 @@ global.crossEnv = queryParam()
 function queryParam () {
   const res = {}
   let [param = null] = process.argv.pop()
-  if (param && /^\-\-.+/test(param)) {
+  if (param && /^\-\-.+/.test(param)) {
     param = param.replace(/\-\-/, '')
     param = param.split('=')
     res[param[0]] = param[1]
