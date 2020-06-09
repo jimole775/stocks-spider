f6185a5c00dd6d7b34a9bc04cf1df20efe2d6ec4
Rongxis <727346595@qq.com>
2020-06-09 18:30:43 +0800

    Update record-peer-deal.js
    处理quest(link)返回null导致的数据不统一问题

--- src/app/base/sniff-daily-deals/record-peer-deal.js
+++ src/app/base/sniff-daily-deals/record-peer-deal.js
@@ -1,10 +1,11 @@
 const path = require('path')
 const { writeFileAsync, quest } = require(`${global.srcRoot}/utils`)
 const recordPath = `${global.srcRoot}/db/warehouse/peer-deals/`
-module.exports = async function recordPeerDeal(stockCode, url) {
+module.exports = async function recordPeerDeal(stockCode, api) {
   try {
     // 修改数据的请求数量?pagesize=144&
-    const dirtyData = await quest(url.replace(/^(http.*?)\?pagesize\=\d*?\&(.*?)$/, '$1?pagesize=99999&$2'))
+    const adjustToMax = api.replace(/^(http.*?)\?pagesize\=\d*?\&(.*?)$/, '$1?pagesize=99999&$2')
+    const dirtyData = await quest(adjustToMax) || '{ "data": { "data": [] }}'
     const pureData = JSON.parse(dirtyData.replace(/^[\w\d_]*?\((.+?)\);$/ig, '$1'))
     return writeFileAsync(path.join(recordPath, global.finalDealDate, `${stockCode}.json`), pureData.data.data)
   } catch (error) {
