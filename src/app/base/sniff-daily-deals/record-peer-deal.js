7eef6d3147b35715dfec5ab74bcff7ed538f7b93
Andy-Super <727346595@163.com>
2020-06-10 22:49:10 +0800

    抓取API数据时，设置默认结果，防止异常

--- src/app/base/sniff-daily-deals/record-peer-deal.js
+++ src/app/base/sniff-daily-deals/record-peer-deal.js
@@ -6,7 +6,7 @@ module.exports = async function recordPeerDeal(stockCode, api) {
     console.log('deal:', stockCode)
     // 修改数据的请求数量?pagesize=144&
     const adjustToMax = api.replace(/^(http.*?)\?pagesize\=\d*?\&(.*?)$/, '$1?pagesize=99999&$2')
-    const dirtyData = await quest(adjustToMax) || '({"data":{"data":[]}})'
+    const dirtyData = await quest(adjustToMax) || 'jquey_123456({"data":{"data":[]}});'
     const pureData = JSON.parse(dirtyData.replace(/^[\w\d_]*?\((.+?)\);$/ig, '$1'))
     return writeFileAsync(path.join(recordPath, global.finalDealDate, `${stockCode}.json`), pureData.data.data)
   } catch (error) {
