5eded8f3a54c61f41c67c9030fa5767a9845a362
Rongxis <727346595@qq.com>
2020-07-16 19:04:20 +0800

    Update main.js

--- src/api/main.js
+++ src/api/main.js
@@ -1,16 +1,19 @@
+require('./config')
 const cmdParam = require('../utils/cmd-param')
 const express = require('express')
 const app = new express()
 const kline = require('./kline')
 const vline = require('./vline')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
-// parse application/x-www-form-urlencoded  
-app.use(bodyParser.urlencoded({ extended: false }))    
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json  
-app.use(bodyParser.json())   
+// parse application/json
+app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 app.post('/api/kline', kline)
 app.post('/api/vline', vline)
 app.listen(cmdParam('port'))
