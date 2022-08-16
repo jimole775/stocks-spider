654ee858f3b6f5b529435843aa73fd007232b2da
16603013472 <rongchuanda@chinasie.com>
2022-08-16 19:23:09 +0800

    feat: bunch-thread fixed task queue overstack

--- test/test1.js
+++ test/test1.js
@@ -1,14 +1,17 @@
-const Bunch = require('F:/my_pro/stocks/src/utils/bunch-thread.js')
-const b = new Bunch(10)
+// const Bunch = require('../src/utils/bunch-thread.js')
+// const b = new Bunch(10)
 
-new Array(100).fill(Math.random()).forEach(async (i) => {
-  await b.taskCalling(() => {
-    return new Promise((resolve) => {
+const data = new Array(100).fill(Math.random())
+
+;(async function () {
+  for (let i = 0; i < data.length; i++) {
+    const el = data[i]
+    console.log('s:', i)
+    await new Promise((resolve) => {
       setTimeout(() => {
         resolve(true)
       }, 1000)
     })
-  })
-})
-
-console.log('end')
+    console.log('end:', i)
+  }
+})()
