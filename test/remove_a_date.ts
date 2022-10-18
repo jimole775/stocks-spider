2bea53d9d07569fb60b9c1c05b30cf9ed79d3811
jimole775 <727346595@qq.com>
2022-10-19 00:14:43 +0800

    feat: updates

--- test/remove_a_date.ts
+++ test/remove_a_date.ts
@@ -1,7 +1,7 @@
-
 import path from 'path'
 import baseData from 'G:\\my_db\\stocks-spider\\base.json'
-import { removeFile } from 'F:/my_pro/stocks/src/utils/remove_file'
+import { removeFile } from 'F:\\pro_javascript\\stocks\\src\\utils\\remove_file'
+
 const db_stocks = `G:\\my_db\\stocks-spider\\stocks`
 const limit = 50
 const date = '2022-10-12.json'
